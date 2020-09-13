d72a6aa527256c34298dc211da28b230294309e4
Kevin Lim <lyskevin@gmail.com>
2020-09-13 15:46:30 +0800

    Add routes for SoundFile, UnopenedMessage, Message, and Conversation to routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,13 @@
 import user from './user';
+import soundFile from './soundFile';
+import unopenedMessage from './unopenedMessage';
+import message from './message';
+import conversation from './conversation';
 
 export default {
   user,
+  soundFile,
+  unopenedMessage,
+  message,
+  conversation,
 };
