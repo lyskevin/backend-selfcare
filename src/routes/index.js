e93927b4508be2f3cdc1fb30f56a5fa9903ba097
Kevin Lim <lyskevin@gmail.com>
2020-09-19 00:03:10 +0800

    Remove unopenedMessage from index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,11 @@
 import user from './user';
 import soundFile from './soundFile';
-import unopenedMessage from './unopenedMessage';
 import message from './message';
 import conversation from './conversation';
 
 export default {
   user,
   soundFile,
-  unopenedMessage,
   message,
   conversation,
 };
