7f9875c14e54d29d4681cb570a3d8ac2a58b7059
Kevin Lim <lyskevin@gmail.com>
2020-09-19 01:23:56 +0800

    Add route for getting random user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
+import { sequelize } from '../models';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
   const allUsers = await req.context.models.User.findAll();
   res.send(allUsers);
+});
+
+router.get('/random', async (req, res) => {
+  const randomUser = await req.context.models.User.findOne({
+    order: sequelize.random(),
+  });
+  res.send(randomUser);
 });
 
 router.get('/:userId', async (req, res) => {
