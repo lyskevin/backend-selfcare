9ae288f3de1cdb7023b652357e5c22856d91b032
Kevin Lim <lyskevin@gmail.com>
2020-09-19 13:08:21 +0800

    Update models/message.js

--- src/models/message.js
+++ src/models/message.js
@@ -25,17 +25,8 @@ const Message = db.define(
 );
 
 Message.associate = (models) => {
-  Message.belongsTo(models.SoundFile, {
-    foreignKey: 'voice_message',
-  });
   Message.belongsTo(models.User, {
     foreignKey: 'user_id',
-  });
-  Message.belongsTo(Message, {
-    foreignKey: 'previous_message',
-  });
-  Message.hasOne(Message, {
-    foreignKey: 'previous_message',
   });
   Message.belongsTo(models.Conversation, {
     foreignKey: 'conversation_id',
