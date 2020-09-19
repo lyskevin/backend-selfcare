5eafd64fa5e382763964a60285afe1ee83eec8b0
Kevin Lim <lyskevin@gmail.com>
2020-09-19 11:42:50 +0800

    Update routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import user from './user';
-import soundFile from './soundFile';
 import message from './message';
 import conversation from './conversation';
 
 export default {
   user,
-  soundFile,
   message,
   conversation,
 };
