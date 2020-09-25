9d30f1a275a0c2608fe08a054f6be19ede856609
Adam Chew <adamwth@hotmail.com>
2020-09-25 18:50:20 +0800

    fix: typo

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,7 +2,7 @@ import models from '../models';
 
 const httpsRedirect = (req, res, next) => {
   if (req.header('x-forwarded-proto') !== 'https')
-    return res.redirect(`https://${req.headers('host')}${req.url}`);
+    return res.redirect(`https://${req.headers.host}${req.url}`);
   next();
 };
 
